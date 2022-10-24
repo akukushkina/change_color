902e6dd17a95c0c3d67d1a1aeb8665856e795349
akyukyushkina <akyukyushkina@gmail.com>
2022-10-24 12:16:02 +0100

    change

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const button = document.querySelector('.change');
-const colors = ['red', 'blue', 'black', 'orange', 'green', 'yellow', 'purple', 'pink'];
+const colorName = document.querySelector('.color__name');
+const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
-button.addEventListener('click', changeColor);
+button.addEventListener('click', function(){
+   let hexColor = '#';
+   for(let i = 0; i < 6; i++){
+     hexColor += colors[changeColor()];
+   }
+   colorName.textContent = hexColor;
+   document.body.style.backgroundColor = hexColor;
+});
 
 function changeColor(){
-   let color = Math.floor(Math.random() * colors.length-1);
-   document.body.style.backgroundColor = colors[color];
+   let color = Math.floor(Math.random() * colors.length);
+   return color;
 }
+
+
 
